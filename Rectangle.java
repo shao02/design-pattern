ddebe9ce62d6096883e01083485dde023b6156f6
shao02 <shao02@users.noreply.github.com>
2016-03-06 23:59:09 -0500

    Added files via upload

---
+++ Rectangle.java
@@ -0,0 +1,8 @@
+package com.factory;
+
+public class Rectangle implements Shape{
+	@Override
+	public void draw(){
+		System.out.print("This is Rectangle");
+	}
+}
---
+++ ShapeFactory.java
@@ -0,0 +1,16 @@
+package com.factory;
+
+public class ShapeFactory {
+	public Shape getShape(String shapeType){
+		if(shapeType == null)
+			return null;
+		else if(shapeType.equals("circle"))
+			return new Circle();
+		else if(shapeType.equals("square"))
+			return new Square();
+		else if(shapeType.equals("rectangle"))
+			return new Rectangle();
+		return null;
+	}
+	
+}
